8f9f0e1c445e68084a6871e5ac16000e7c8eb6f8
Xavimat <xaviermatoses@gmail.com>
2022-05-09 16:09:59 +0200

    Functionality

--- asyncawait.js
+++ asyncawait.js
@@ -13,7 +13,8 @@ const MAX_POKS = 1126;
 ////////////////////////////////////////////////////////////////////////////////
 // DOM
 const pokeBall = document.querySelector('#poke-ball');
-const pokeDiv = document.querySelector('#poke-div');
+const pokeH1 = document.querySelector('#poke-h1');
+const pokeImg = document.querySelector('#poke-img');
 
 ////////////////////////////////////////////////////////////////////////////////
 // Globals
@@ -25,11 +26,41 @@ const pokeDiv = document.querySelector('#poke-div');
 
 ////////////////////////////////////////////////////////////////////////////////
 // Utils
-
+const getRandomInt = (num) => Math.floor(Math.random() * num);
+const putSpinner = () => {
+    pokeH1.innerHTML = '<h1>&#8635;</h1>';
+    pokeImg.src = './assets/img/empty.png'
+}
+const putName = (name) => pokeH1.innerHTML = '<h1>'+name+'</h1>';
+const putImg = (url) => pokeImg.src = url;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Functions
+const getImg = async (pokeData) => {
+    try {
+        const poke = await axios(pokeData.url);
+        const pokeImgUrl = poke.data.sprites.front_default;
+        putImg(pokeImgUrl);
 
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const catchEmAll = async () => {
+    putSpinner();
+    const randId = getRandomInt(MAX_POKS);
+    try {
+        const res = await axios(APIURL + randId);
+        const pokeData = res.data.results[0];
+        // console.log(pokeData);
+        putName(pokeData.name);
+        getImg(pokeData);
+
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 // Listeners
